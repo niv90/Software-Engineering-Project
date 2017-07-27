ba58faa717de65f63f8a57e31c2b23a0d7ffa29c
Alaa Halumi <alaa.halumi@gmail.com>
2017-07-27 22:50:36 +0300

    chaning the main for check checklist

--- Shitot/Source.cpp
+++ Shitot/Source.cpp
@@ -4,11 +4,14 @@
 #include <vector>
 #include "../Panel/Panel.h"
 #include "../TextBox/TextBox.h"
+#include "../CheckList/CheckList.h"
 
 
 using namespace std;
 
 int main(void) {
+
+	vector<string> stringsA = { "Good", "Day", "Whats", "Up" };
 
 	Panel panel(60, 75);
 	panel.setLocationX(2);
@@ -18,7 +21,11 @@ int main(void) {
 	tb.SetBackground(BackgroundColor::Teal);
 	tb.SetForeground(ForegroundColor::White);
 
+	CheckList checkList(15, 14, stringsA);
+	checkList.SetBackground(BackgroundColor::Green);
+
 	panel.AddControl(&tb, 43, 20);
+	panel.AddControl(&checkList, 2, 2);
 	
 	panel.setVisible(true);
 	
